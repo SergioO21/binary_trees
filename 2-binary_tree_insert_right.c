58f7109e3fe393236986e2e486260dc2f68b4bae
jblanco75 <jblanco75@gmail.com>
2021-07-21 22:20:34 +0000

    fix attempt

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,13 +23,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->right)
 	{
-		new_node->parent = parent;
 		new_node->right = parent->right;
 		parent->right = new_node;
 	}
